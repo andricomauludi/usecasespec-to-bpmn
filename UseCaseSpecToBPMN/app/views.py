81e92907320871df9c1a9d97a760b07f66b700a8
Andrico unair <andrico.mauludi.junianto-2018@fst.unair.ac.id>
2021-05-30 22:34:36 +0700

    (ico) repair add nya register

--- UseCaseSpecToBPMN/app/views.py
+++ UseCaseSpecToBPMN/app/views.py
@@ -40,7 +40,7 @@ def registerPage(request):
             form.save()
             user = form.cleaned_data.get('username')
             messages.success(request, 'Account was created for '+user)
-            return redirect('/userlist')
+            return redirect('/login')
 
     context={'form':form}
     return render(request,'register.html',context)
