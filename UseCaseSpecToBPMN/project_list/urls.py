158a783f9162c35a74f8f61cc6da0e771135f9f9
albsam36 <Acalbertsamuel123@gmail.com>
2021-06-06 17:39:27 +0700

    Generate BPMN

--- UseCaseSpecToBPMN/project_list/urls.py
+++ UseCaseSpecToBPMN/project_list/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from app import views as blogViews
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -11,4 +12,5 @@ urlpatterns = [
     path('scenario_list/scenario_list_form/', views.createscenario),
     path('scenario_list/scenario_update/<str:pk>', views.updatescenario),
     path('scenario_list/scenario_delete/<str:pk>', views.deletescenario),
+    path('Generate/', views.Generate),
 ]
--- UseCaseSpecToBPMN/project_list/views.py
+++ UseCaseSpecToBPMN/project_list/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from app.models import projectlist, scenariolist
 from app.forms import projectform, scenarioform
+import xml.etree.ElementTree as ET
 # Create your views here.
 
 def index(request):
@@ -60,3 +61,65 @@ def deletescenario(request,pk):
     hantu = scenariolist.objects.filter(no=pk).values_list('projectno_id', flat=True).first()
     scenariodel.delete()
     return redirect('index', no=scenariodel.projectno_id)
+
+def Generate(request):
+    resultsdisplay=scenariolist.objects.all
+    for x in resultsdisplay():
+        y = str(x.scenarioid)
+        panjangx= str(x.scenarioid*150)
+        if x.scenarioid == 1:
+            filexml = ".\staticfiles\mediafiles\sbpmnawal.bpmn"
+            tree = ET.parse(filexml)
+            root = tree.getroot()
+            action = root.find(".//process")
+            doc = ET.SubElement(action, "task")
+            doc.set('id','acsajja'+y)
+            doc.set('name',x.scenario)
+            act = root.find(".//task")
+            b = ET.SubElement(act, 'Description')
+            act1 = root.find(".//{http://www.omg.org/spec/BPMN/20100524/DI}BPMNPlane")
+            doc1 = ET.SubElement(act1, "{http://www.omg.org/spec/BPMN/20100524/DI}BPMNShape")
+            doc1.set('id','acsajja'+y+'_di')
+            IDSHAPE= 'acsajja'+y+'_di'
+            print(IDSHAPE)
+            doc1.set("bpmnElement",'acsajja'+y)
+            act1 = root.find('.//{http://www.omg.org/spec/BPMN/20100524/DI}BPMNShape/[@id="%s"]' %IDSHAPE)
+            print(act1)
+            c = ET.SubElement(act1, '{http://www.omg.org/spec/DD/20100524/DC}Bounds')
+            c.set('x',panjangx)
+            c.set('y','100')
+            c.set('width','57')
+            c.set('height','40')
+            tree.write(".\staticfiles\mediafiles\hasil7.bpmn")
+        else:
+            filexml = ".\staticfiles\mediafiles\hasil7.bpmn"
+            tree = ET.parse(filexml)
+            root = tree.getroot()
+            print(root)
+            action = root.find(".//process")
+            print(action)
+            doc = ET.SubElement(action, "task")
+            doc.set('id', 'acsajja'+y)
+            idtask = 'acsajja'+y
+            print(idtask)
+            doc.set('name',x.scenario)
+            act = root.find('.//task[@id="%s"]' %idtask)
+            b = ET.SubElement(act, 'Description')
+            act1 = root.find(".//{http://www.omg.org/spec/BPMN/20100524/DI}BPMNPlane")
+            doc1 = ET.SubElement(act1, "{http://www.omg.org/spec/BPMN/20100524/DI}BPMNShape")
+            doc1.set('id', 'acsajja'+y+'_di')
+            idShape = 'acsajja'+y+'_di'
+            doc1.set("bpmnElement", 'acsajja'+y)
+            act1 = root.find('.//{http://www.omg.org/spec/BPMN/20100524/DI}BPMNShape/[@id="%s"]' %idShape)
+            c = ET.SubElement(act1, '{http://www.omg.org/spec/DD/20100524/DC}Bounds')
+            c.set('x', panjangx)
+            c.set('y', '100')
+            c.set('width', '57')
+            c.set('height', '40')
+            tree.write(".\staticfiles\mediafiles\hasil7.bpmn")
+    filexml = ".\staticfiles\mediafiles\hasil7.bpmn"
+    tree = ET.parse(filexml)
+    root = tree.getroot()
+    root.set('xmlns',"http://www.omg.org/spec/BPMN/20100524/MODEL")
+    tree.write(".\staticfiles\mediafiles\hasil7.bpmn")
+    return render(request, 'coba.html')
